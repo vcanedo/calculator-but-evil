5faf6a0394b20762f6bdadc570a63b9640341a40
victor canedo <victorcanedo1@hotmail.com>
2023-08-14 15:34:36 -0300

    added sound function to js

--- js/calculator.js
+++ js/calculator.js
@@ -12,9 +12,9 @@ function evilResponse(result) {
       "Did you really think you'd get anything different? " + result,
       "Witness the fruit of your labor: " + result,
       "I hope your self-esteem can handle this: " + result
-  ];
+    ];
 
-  return evilResponses[Math.floor(Math.random() * evilResponses.length)];
+    return evilResponses[Math.floor(Math.random() * evilResponses.length)];
 }
 
 let currentResult = '';  // New variable to store the current result
@@ -22,50 +22,75 @@ let previousResult = ''; // New variable to store the previous result
 
 // function to append the value to the result
 function appendToResult(value) {
-    document.getElementById('result').value += value;
+  document.getElementById('result').value += value;
 }
 
 // function to clear the result
 function clearResult() {
-    document.getElementById('result').value = '';
+  document.getElementById('result').value = '';
 }
+
+// //////////////////////////////////////////////////
+// SOUND EFFECTS
+// Get the audio elements
+
+const buttonClickSound = document.getElementById('button-click-sound');
+const resultSound = document.getElementById('result-sound');
+
+// Define the sound functions
+function playButtonClickSound() {
+  buttonClickSound.currentTime = 0; // Reset sound to start
+  buttonClickSound.play();
+}
+
+function playResultSound() {
+  resultSound.currentTime = 0; // Reset sound to start
+    resultSound.play();
+  }
+
+  // Attach the sound functions to button clicks
+  document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', playButtonClickSound);
+  });
 
 // function to calculate the result
 function calculate() {
-    previousResult = currentResult; // Store the current result before calculation
-    const expression = document.getElementById('result').value;
-    try {
+  previousResult = currentResult; // Store the current result before calculation
+  const expression = document.getElementById('result').value;
+  try {
         currentResult = eval(expression); // Calculate the current result
         const response = evilResponse(currentResult); // Get an evil response
         document.getElementById('result').value = response;
+        playResultSound(); // Play sound effect
     } catch (error) {
         document.getElementById('result').value = 'Error';
+      }
     }
-}
 
-// New function to show the previous result
-function showPreviousResult() {
-    document.getElementById('result').value = previousResult;
-}
+    // New function to show the previous result
+    function showPreviousResult() {
+      document.getElementById('result').value = previousResult;
+    }
 
-// KEYBOARD SUPPORT
-// Listen for keydown events on the document
-document.addEventListener('keydown', function(event) {
-  const key = event.key;
+    // //////////////////////////////////////////////////
+    // KEYBOARD SUPPORT
+    // Listen for keydown events on the document
+    document.addEventListener('keydown', function(event) {
+      const key = event.key;
 
-  // If the key is a number, an operator, or Enter (=), append it to the result
-  if (/^\d$|[\+\-\*/]|Enter/.test(key)) {
+      // If the key is a number, an operator, or Enter (=), append it to the result
+    if (/^\d$|[\+\-\*/]|Enter/.test(key)) {
       event.preventDefault(); // Prevent the default action of the key press
       if (key === 'Enter') {
-          calculate();
+        calculate();
       } else {
-          appendToResult(key);
+        appendToResult(key);
       }
   }
 
   // Clear the result when the Escape key is pressed
   if (key === 'Escape') {
-      clearResult();
+    clearResult();
   }
 
   // Toggle theme when the T key is pressed
@@ -77,5 +102,6 @@ document.addEventListener('keydown', function(event) {
   // Show previous result when the P key is pressed
   if (key === 'p' || key === 'P') {
       showPreviousResult();
-  }
+    }
 });
+
